0fb4d71fe8ea31e8037f89bf278c3e64adccecd9
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2024-01-19 15:42:22 -0600

    Vercel

--- database.js
+++ database.js
@@ -1,14 +1,24 @@
 let DATABASE_URL = {
-    host: "aws.connect.psdb.cloud",
-    user: "2cl8pili501kc9hyecsn",
-    password: "pscale_pw_1vPairM2izLkhuYwP2WIFNDotKCll4EfqKAINkpdP5q",
     database: "node",
+    username: "w1i4143lez22u2l0ntih",
+    host: "aws.connect.psdb.cloud",
+    password: "pscale_pw_WCcDLS5Sh1JTzAYaYpjx2wlDgCi6dUnAGAloVAzCXxE"
 };
 
-// Crear la URL de conexión
-let config = `mysql://${DATABASE_URL.user}:${DATABASE_URL.password}@${DATABASE_URL.host}/${DATABASE_URL.database}?ssl={"rejectUnauthorized":true}`;
+// Crear la URL de conexión con configuración SSL
+let config = {
+    host: DATABASE_URL.host,
+    user: DATABASE_URL.username,
+    password: DATABASE_URL.password,
+    database: DATABASE_URL.database,
+    ssl: {
+        rejectUnauthorized: true
+    }
+};
 
-// Exportar la URL de conexión
+// Convertir el objeto config a una cadena de conexión URI
+let DATABASE_URL_STRING = `mysql://${config.user}:${config.password}@${config.host}/${config.database}?ssl=${encodeURIComponent(JSON.stringify(config.ssl))}`;
 
-
+// Exportar la configuración de conexión y la cadena de conexión URI
 exports.config = config;
+exports.DATABASE_URL = DATABASE_URL_STRING;
