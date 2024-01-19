0596d354e1c22befbd645cc92803c43801b4ab28
Alexzeuqram <101904746+Alexzeuqram@users.noreply.github.com>
2024-01-19 06:17:03 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
-let config = {
-    host: "aws.connect.psdb.cloud",
-    user: "51bsvrthdtdefbbn43cu",
-    password: "pscale_pw_8LRvF8prkNnl9VGc3uBwcNnULiF96VoaXo7vCcBNpjI",
-    database: "node",
+let DATABASE_URL = {
+database: node,
+username: jup11qrb1oc1z2ybu14h,
+host: aws.connect.psdb.cloud,
+password: pscale_pw_ct6odQjzhk7DusfDchmCIlylKxNFt154AjUfA5oNFCJ
+
 };
 
-
+let config = `mysql://${DATABASE_URL.username}:${DATABASE_URL.password}@${DATABASE_URL.host}/${DATABASE_URL.database}?ssl={"rejectUnauthorized":true}`;
 
 
 exports.config = config;
