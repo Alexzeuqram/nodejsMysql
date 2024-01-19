0724999c9472592d176ae76760c921517cf1ffcf
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2024-01-19 17:04:14 -0600

    Nuevo

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
-let DATABASE_URL = {
-    database: "node",
-    user: "u2yz42m01piwcn7tjkbh",
-    host: "aws.connect.psdb.cloud",
-    password: "pscale_pw_7MC51DksvyChzJO5cWresZSDdtHNbKrPUXD4zcA7KSc"
+let config = {
+    database: "bgkmqh87aclkisnzn7le",
+user: "um9ktbrd9laara6m",
+host: "bgkmqh87aclkisnzn7le-mysql.services.clever-cloud.com",
+password: "DX4MFJh5mrL7Ho5XeZ9F"
+  
   
   };
-  
-  let config = `mysql://${DATABASE_URL.user}:${DATABASE_URL.password}@${DATABASE_URL.host}/${DATABASE_URL.database}?ssl={"rejectUnauthorized":true}`;
-  
+
+ 
   exports.config = config;
+  
   
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express"),
+
   app = express(),
   puerto = process.env.PORT || 3000;
   (mysql = require("mysql")),
   (bodyParser = require("body-parser")),
   (myConnection = require("express-myconnection")),
   (db = require("./database").config);
+ 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(myConnection(mysql, db));
