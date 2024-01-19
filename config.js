4a0302387510482f509862727a346a61a26bc745
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2024-01-19 05:56:33 -0600

    Actualizacion bases

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || '',
-    DB_NAME: process.env.DB_NAME || 'mi_blog',
-    DB_PORT: process.env.DB_PORT || 3306
-};
--- database.js
+++ database.js
@@ -1,12 +1,14 @@
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = require("./config");
-
-
-
-let config = {
-	host: DB_HOST,
-	user: DB_USER,
-	password: DB_PASSWORD,
-	port: DB_PORT,
-	database: DB_NAME
+let DATABASE_URL = {
+    host: "aws.connect.psdb.cloud",
+    user: "2cl8pili501kc9hyecsn",
+    password: "pscale_pw_1vPairM2izLkhuYwP2WIFNDotKCll4EfqKAINkpdP5q",
+    database: "node",
 };
+
+// Crear la URL de conexión
+let config = `mysql://${DATABASE_URL.user}:${DATABASE_URL.password}@${DATABASE_URL.host}/${DATABASE_URL.database}?ssl={"rejectUnauthorized":true}`;
+
+// Exportar la URL de conexión
+
+
 exports.config = config;
