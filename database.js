aa4d4771d146475306a3d70315f0702dcc3eb462
Alexzeuqram <jared.marquez.is@unipolidgo.edu.mx>
2024-01-19 16:08:14 -0600

    act

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 let DATABASE_URL = {
     database: "node",
-    user: "3vfc8yg1hraub6zumjx4", // Cambiado de "username" a "user"
+    user: "u2yz42m01piwcn7tjkbh",
     host: "aws.connect.psdb.cloud",
-    password: "pscale_pw_1qtjdi5mce8bweOivBjyccGjdg5XHgHK4Q8ty8mbwsE"
+    password: "pscale_pw_7MC51DksvyChzJO5cWresZSDdtHNbKrPUXD4zcA7KSc"
+  
   };
   
   let config = `mysql://${DATABASE_URL.user}:${DATABASE_URL.password}@${DATABASE_URL.host}/${DATABASE_URL.database}?ssl={"rejectUnauthorized":true}`;
