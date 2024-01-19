c3755c7d60e5d8b5127a0297aa5787569df9496d
Alexzeuqram <101904746+Alexzeuqram@users.noreply.github.com>
2024-01-19 06:13:02 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-let DATABASE_URL = {
+let config = {
     host: "aws.connect.psdb.cloud",
     user: "51bsvrthdtdefbbn43cu",
     password: "pscale_pw_8LRvF8prkNnl9VGc3uBwcNnULiF96VoaXo7vCcBNpjI",
@@ -6,11 +6,6 @@ let DATABASE_URL = {
 };
 
 
-
-// Crear la URL de conexión
-let config = `mysql://${DATABASE_URL.user}:${DATABASE_URL.password}@${DATABASE_URL.host}/${DATABASE_URL.database}?ssl={"rejectUnauthorized":true}`;
-
-// Exportar la URL de conexión
 
 
 exports.config = config;
